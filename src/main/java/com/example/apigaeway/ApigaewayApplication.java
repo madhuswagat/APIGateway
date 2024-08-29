ecbf63414f17ce9047ffa6baf3ad067a47ecb1af
madhu9378 <madhuswagat2006@gmail.com>
2024-08-29 11:36:11 +0530

    Api Gateway

---
+++ src/main/java/com/example/apigaeway/ApigaewayApplication.java
@@ -0,0 +1,13 @@
+package com.example.apigaeway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApigaewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApigaewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/apigaeway/ApigaewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.apigaeway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApigaewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
